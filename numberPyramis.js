0d79ebf960cad765a440d73702b8e2925e2931d1
adouch9 <mr.ouldmoussa@gmail.com>
2021-03-12 17:33:34 +0100

    exercices

--- numberPyramis.js
+++ numberPyramis.js
@@ -23,17 +23,3 @@ const numberPyramid = (number) => {
 
 
 
-
-// const pyramid = (nbElement, renverse, str) => {
-//     if(!renverse) {
-//       for(let i = 0; i <= nbElement; i++) {
-//         console.log(str.repeat(i));
-//       } 
-//     } else {
-//       for(let i = nbElement; i >= 1; i--) {
-//         console.log(str.repeat(i))
-//       }
-//     }
-//   }
-  
-//   (pyramid(10, false, "A"))
